c6ab0b9a96c36f8f75d74b23e344b1e26354f2c8
Jeaanca <jeaanca.santos@gmail.com>
2020-05-16 21:26:10 -0300

    Added inserts to tables account, contacts, profile

---
+++ schema/contacts.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.contacts (user_id,contact_id) VALUES 
+(8,10)
+,(8,11)
+,(8,5)
+,(11,10)
+,(11,8)
+,(10,8)
+,(10,11)
+;
---
+++ schema/profile.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.profile (full_name,phone,description,image) VALUES 
+('Jean Carlos',NULL,'Analista Programador','images/jean.jpg')
+,('Maria',NULL,'Descrição da Maria ','images/maria.jpg')
+,('Pedro',NULL,'Usuario pedro','images/pedro.jpg')
+;
