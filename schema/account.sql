262b8601d7f1a059efea50fe71940cf0076599d4
Jeaanca <jeaanca.santos@gmail.com>
2020-05-24 14:06:21 -0300

    Removed schemas

--- schema/account.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.account (username,"password",email,created_on,last_login,profile_id) VALUES 
-('jean','$2b$10$JMaxpcJIGx9MKxjZYdvIL.C7IIpyosXkFjEPndqK.LHq/VIgkgclm','jeaanca@sitecom','2020-03-21 00:00:00.000',NULL,1)
-,('admin','$2b$10$JMaxpcJIGx9MKxjZYdvIL.C7IIpyosXkFjEPndqK.LHq/VIgkgclm','admin@site.com','2020-03-21 00:00:00.000',NULL,NULL)
-,('maria','$2b$10$JMaxpcJIGx9MKxjZYdvIL.C7IIpyosXkFjEPndqK.LHq/VIgkgclm','maria@site.com','2020-03-21 00:00:00.000',NULL,3)
-,('pedro','$2b$10$JMaxpcJIGx9MKxjZYdvIL.C7IIpyosXkFjEPndqK.LHq/VIgkgclm','pedro@site.com','2020-03-21 00:00:00.000',NULL,4)
-;
--- schema/contacts.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public.contacts (user_id,contact_id) VALUES 
- (1,3)
-,(1,4)
-,(3,4)
-;
