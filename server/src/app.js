6c7c6cc14e27086f1881319f2e9346dede4191f6
Jeaanca <jeaanca.santos@gmail.com>
2020-05-30 23:07:49 -0300

    Added cors in socket.io

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,17 @@ import preflight from './methods/preflight'
 
 const app = express()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+  handlePreflightRequest: (req, res) => {
+      const headers = {
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Origin": req.headers.origin,
+          "Access-Control-Allow-Credentials": true
+      };
+      res.writeHead(200, headers);
+      res.end();
+  }
+})
 
 app.use(preflight.setHeaders)
 
